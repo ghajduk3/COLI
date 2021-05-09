30f3962202496c1cb85a9ca0f5d9432aa201dc3b
ghajduk3 <gh8590@student.uni-lj.si>
2021-05-09 22:01:02 +0200

    SVM and XGBoost support for topic modelling

--- classifiers/xg_boost.py
+++ classifiers/xg_boost.py
@@ -37,13 +37,13 @@ def setup_classifier(x_train, y_train,features="preprocessed", method="count", n
     """
 
     if method == "count":
-        vec, x_train = combine_features(features, x_train, method=method, ngramrange=ngrams)
+        vec, x_train, topic_model_dict = combine_features(features, x_train, method=method, ngramrange=ngrams)
     elif method == "tfidf":
-        vec, x_train = combine_features(features, x_train, method=method, ngramrange=ngrams)
+        vec, x_train, topic_model_dict = combine_features(features, x_train, method=method, ngramrange=ngrams)
     else:
         print("Method has to be either count or tfidf")
         return 1
 
     xgboost_classifier = xgb.XGBClassifier(learning_rate = 0.05, n_estimators=300, max_depth=5,random_state=42)
     model = xgboost_classifier.fit(x_train, y_train.values.ravel())
-    return model, vec
+    return model, vec, topic_model_dict
